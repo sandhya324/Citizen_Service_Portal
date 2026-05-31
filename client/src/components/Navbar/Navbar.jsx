b12690287600f3571b07db071034ce6da77c63b5
sandhya324 <sdhya3842@gmail.com>
2026-05-31 12:26:35 +0530

    build navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -6,7 +6,18 @@ const Navbar = () => {
   return (
     
     <div className="navbar">
-      <img src={assets.logo} alt="Logo" className="Logo"/>
+
+
+      <div className="navbar-left">
+         <img src={assets.logo} alt="Logo" />
+
+         <div className="logo-text">
+          <p>Citizen Service Portal</p>
+          <span>Government of India</span>
+         </div>
+        
+      </div>
+        
 
       <ul className="navbar-links">
         <li>Home</li>
@@ -14,9 +25,16 @@ const Navbar = () => {
         <li>Register Complaint</li>
         <li>Track Status</li>
         <li>Contact Us</li>
-        <li>Login</li>
-        <li>Register</li>
       </ul>
+
+
+      <div className="navbar-right">
+
+        <button>Login</button>
+        <button>Register</button>
+    
+      </div>
+
 
     </div>
 
