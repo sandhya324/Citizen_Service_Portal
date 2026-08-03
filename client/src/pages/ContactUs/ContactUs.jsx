33fe7843998a95d40872f854d8de1d60b0074f73
sandhya324 <sdhya3842@gmail.com>
2026-08-03 18:56:45 +0530

    Making it responsible

--- client/src/pages/ContactUs/ContactUs.jsx
+++ client/src/pages/ContactUs/ContactUs.jsx
@@ -115,7 +115,7 @@ const ContactUs = () => {
 
 
       <div className="loc-map">
-        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5714334.612218336!2d75.17250453317214!3d29.590137939153575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1784182493228!5m2!1sen!2sin" width="750" height="280" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="strict-origin-when-cross-origin"></iframe>        
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5714334.612218336!2d75.17250453317214!3d29.590137939153575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390cfd5b347eb62d%3A0x37205b715389640!2sDelhi!5e0!3m2!1sen!2sin!4v1784182493228!5m2!1sen!2sin" width="100%" height="280" style={{ border: 0 }} allowFullScreen="" loading="lazy" referrerPolicy="strict-origin-when-cross-origin"></iframe>        
       </div>
 
      </div>
